1f600f67c33b487d89d92b4cb148180c27cc31e5
Casilda Rufina <casildapaul25@gmail.com>
2025-03-25 13:19:20 +0530

    Delete tailwind.config.js

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  darkMode: 'class',  // Enable dark mode
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
